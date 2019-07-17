22bb68d7e7265dc8157ccf04800eb0b281b3c558
avvinci <vinciabhinav7@gmail.com>
2019-07-17 09:16:50 +0530

    pm2 working :)

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ import cron from "node-cron";
 import fetchingData from "./fetch";
 import wallpaper from "wallpaper";
 
-cron.schedule("10 * * * * *", () => {
+cron.schedule("* * * * *", () => {
   console.log("yo");
   fetchingData();
   (async () => {
     await wallpaper.set("image.jpg");
   })();
-  //   setWallpaper();
   console.log("..done");
 });
-
-function setWallpaper() {}
+// pm2 start index.js --node-args="--require esm"
